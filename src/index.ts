a10c9f25e9fb5843bbddec4638f40877a8b4492f
Alberto Valdez <albertovaldez.dev@gmail.com>
2024-05-07 17:13:27 -0600

    add cors compression and bodyparser

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import express from 'express'
 import ediblesRoute from './routes/edibles'
+import bodyParser from 'body-parser'
+import compression from 'compression'
+import cors from 'cors'
 
 const app = express()
-
-app.use(express.json())
+app.use(compression())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors({
+  credentials: true
+}))
 
 const PORT = 3000
 
