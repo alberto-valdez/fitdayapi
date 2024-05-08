fe25d846200b9687a09588b96f4d5519311bcdb7
Alberto Valdez <albertovaldez.dev@gmail.com>
2024-05-08 16:32:01 -0600

    add params request

--- src/controllers/edibles.ts
+++ src/controllers/edibles.ts
@@ -1,7 +1,7 @@
-import { RequestHandler } from 'express'
+import { RequestHandler, Response, Request } from 'express'
 import { handleCreateEdible } from '../services/edibles'
 
-export const createEdible: RequestHandler = (req, res) => {
+export const createEdible: RequestHandler = (req: Request, res: Response) => {
   const data = handleCreateEdible(req.body)
   res.send(data)
 }
