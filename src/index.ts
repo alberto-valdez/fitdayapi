10ddbd6e21ac91a6fd506fec91984f05e5a09e56
Alberto Valdez <albertovaldez.dev@gmail.com>
2024-05-09 14:12:11 -0600

    init connection to mongo

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import express from 'express'
 import ediblesRoute from './routes/edibles'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import db from './utils/db'
 
 const app = express()
-
 app.use(cors({
   credentials: true
 }))
@@ -14,12 +14,10 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 const PORT = 3000
 
-app.get('/api/edible/hello', (_req, res) => {
-  res.send('hello world')
-})
-
 app.use('/api/edible', ediblesRoute)
 
-app.listen(PORT, () => {
-  console.log(`server on ${PORT}`)
+void db.connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server on ${PORT}`)
+  })
 })
