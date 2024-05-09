56322aba37e39513048ad3806935efa3e2eef62b
Alberto Valdez <albertovaldez.dev@gmail.com>
2024-05-09 10:29:15 -0600

    add validators

--- src/controllers/edibles.ts
+++ src/controllers/edibles.ts
@@ -1,7 +1,18 @@
-import { RequestHandler, Response, Request } from 'express'
+import { Response, Request } from 'express'
 import { handleCreateEdible } from '../services/edibles'
+import { FieldValidationError, Result, validationResult } from 'express-validator'
 
-export const createEdible: RequestHandler = (req: Request, res: Response) => {
-  const data = handleCreateEdible(req.body)
-  res.send(data)
+export const createEdible = (req: Request, res: Response): any => {
+  try {
+    const err: Result = validationResult(req)
+    if (!err.isEmpty()) {
+      const errorData: FieldValidationError[] = err.array()
+      const pathMessage: string = errorData[0].path
+      res.status(404).send(`Invalid or missing value ${pathMessage}`)
+    }
+    const data = handleCreateEdible(req.body)
+    res.send(data)
+  } catch (error) {
+    res.status(500).send('Something went wrong')
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import express from 'express'
 import ediblesRoute from './routes/edibles'
-import bodyParser from 'body-parser'
-import compression from 'compression'
 import cors from 'cors'
+import bodyParser from 'body-parser'
 
 const app = express()
-app.use(compression())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+
 app.use(cors({
   credentials: true
 }))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const PORT = 3000
 
